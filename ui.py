4cd924d185f497f0385566d6976c3046d1f07d73
Modail <919471638@qq.com>
2021-06-24 16:54:45 +0800

    aes python

--- ui.py
+++ ui.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import tkinter as tk
+import tkinter
 import pyaes
 import time
+import os
 from tkinter import *
 from tkinter import filedialog
+import tkinter.messagebox
+import tkinter.simpledialog
 
 
 class Application(tk.Tk):
@@ -48,10 +52,14 @@ class Application(tk.Tk):
         cbcdecryptbutton = tk.Button(cbcframe,
                                      command=self.__cbcdecrypt,
                                      text='解密')
+        cbc_changekey = tk.Button(cbcframe,
+                                  command=self._cbcchangepw,
+                                  text='修改密码')                             
         cbc_entimelabel = tk.Label(cbcframe, text='加密所花时间(s):')
         cbc_entimeshow = tk.Label(cbcframe, textvariable=self.cbc_entime)
         cbc_detimelabel = tk.Label(cbcframe, text='解密所花时间(s):')
         cbc_detimeshow = tk.Label(cbcframe, textvariable=self.cbc_detime)
+        
         # cbc放置位置
         cbclabel.grid(row=0, column=0)
         cbcflabel.grid(row=1, column=0)
@@ -60,7 +68,8 @@ class Application(tk.Tk):
         cbcklabel.grid(row=2, column=0)
         cbckeyentry.grid(row=2, column=1)
         cbcencryptbutton.grid(row=2, column=2, padx=5)
-        cbcdecryptbutton.grid(row=2, column=3)
+        cbcdecryptbutton.grid(row=2, column=3, padx=5)
+        cbc_changekey.grid(row=2,column=4)
         cbc_entimelabel.grid(row=3, column=0)
         cbc_entimeshow.grid(row=3, column=1)
         cbc_detimelabel.grid(row=4, column=0)
@@ -78,10 +87,14 @@ class Application(tk.Tk):
         cfbdecryptbutton = tk.Button(cfbframe,
                                      command=self.__cfbdecrypt,
                                      text='解密')
+        cfb_changekey = tk.Button(cfbframe,
+                                  command=self._cfbchangepw,
+                                  text='修改密码')
         cfb_entimelabel = tk.Label(cfbframe, text='加密所花时间(s):')
         cfb_entimeshow = tk.Label(cfbframe, textvariable=self.cfb_entime)
         cfb_detimelabel = tk.Label(cfbframe, text='解密所花时间(s):')
         cfb_detimeshow = tk.Label(cfbframe, textvariable=self.cfb_detime)
+
         # cfb放置位置
         cfblabel.grid(row=0, column=0)
         cfbflabel.grid(row=1, column=0)
@@ -90,7 +103,8 @@ class Application(tk.Tk):
         cfbklabel.grid(row=2, column=0)
         cfbkeyentry.grid(row=2, column=1)
         cfbencryptbutton.grid(row=2, column=2, padx=5)
-        cfbdecryptbutton.grid(row=2, column=3)
+        cfbdecryptbutton.grid(row=2, column=3, padx=5)
+        cfb_changekey.grid(row=2,column=4)
         cfb_entimelabel.grid(row=3, column=0)
         cfb_entimeshow.grid(row=3, column=1)
         cfb_detimelabel.grid(row=4, column=0)
@@ -107,6 +121,9 @@ class Application(tk.Tk):
         startime = time.perf_counter()  #调用time库获得加密开始时间
         cbcbytekey = self.cbcentrykeyvar.get().encode(
             'utf-8')  #通过get()将stringvar类型变成string再编码
+        if len(cbcbytekey) != 16 and len(cbcbytekey) != 24 and len(cbcbytekey) != 32:
+            tkinter.messagebox.showwarning('警告','密钥长度非法')
+            return
         cbcmode = pyaes.AESModeOfOperationCBC(cbcbytekey)
         file_in = open(self.cbcentryfilevar.get(),
                        'rb')  #加b以二进制形式读写，bytes兼容py2、py3
@@ -120,8 +137,11 @@ class Application(tk.Tk):
     def __cbcdecrypt(self):
         startime = time.perf_counter()
         cbcbytekey = self.cbcentrykeyvar.get().encode('utf-8')
+        if len(cbcbytekey) != 16 and len(cbcbytekey) != 24 and len(cbcbytekey) != 32:
+            tkinter.messagebox.showwarning('警告','密钥长度非法')
+            return
         cbcmode = pyaes.AESModeOfOperationCBC(cbcbytekey)
-        file_in = open('./passwdency_cbc.txt', 'rb')
+        file_in = open(self.cbcentryfilevar.get(), 'rb')
         file_out = open('./passwddecy_cbc.txt', 'wb')
         pyaes.decrypt_stream(cbcmode, file_in, file_out)
         file_in.close()
@@ -138,6 +158,9 @@ class Application(tk.Tk):
     def __cfbencrypt(self):
         startime = time.perf_counter()
         cfbbytekey = self.cfbentrykeyvar.get().encode('utf-8')
+        if len(cfbbytekey) != 16 and len(cfbbytekey) != 24 and len(cfbbytekey) != 32:
+            tkinter.messagebox.showwarning('警告','密钥长度非法')
+            return
         cfbmode = pyaes.AESModeOfOperationCFB(cfbbytekey,
                                               iv=None,
                                               segment_size=8)
@@ -152,16 +175,75 @@ class Application(tk.Tk):
     def __cfbdecrypt(self):
         startime = time.perf_counter()
         cfbbytekey = self.cfbentrykeyvar.get().encode('utf-8')
+        if len(cfbbytekey) != 16 and len(cfbbytekey) != 24 and len(cfbbytekey) != 32:
+            tkinter.messagebox.showwarning('警告','密钥长度非法')
+            return
         cfbmode = pyaes.AESModeOfOperationCFB(cfbbytekey,
                                               iv=None,
                                               segment_size=8)
-        file_in = open('./passwdency_cfb.txt', 'rb')
+        file_in = open(self.cfbentryfilevar.get(), 'rb')
         file_out = open('./passwddecy_cfb.txt', 'wb')
         pyaes.decrypt_stream(cfbmode, file_in, file_out)
         file_in.close()
         file_out.close()
         endtime = time.perf_counter()
         self.cfb_detime.set(endtime - startime)
+    
+    def _cbcchangepw(self):
+        '''先使用旧密码解密'''
+        cbcbytekey = self.cbcentrykeyvar.get().encode('utf-8')
+        cbcmode = pyaes.AESModeOfOperationCBC(cbcbytekey)
+        if len(cbcbytekey) != 16 and len(cbcbytekey) != 24 and len(cbcbytekey) != 32:
+            tkinter.messagebox.showwarning('警告','密钥长度非法')
+            return
+        file_in = open(self.cbcentryfilevar.get(), 'rb')
+        file_out = open('./temp.txt', 'wb')
+        pyaes.decrypt_stream(cbcmode, file_in, file_out)
+        file_in.close()
+        file_out.close()
+        '''使用新密码加密'''
+        newkey = tkinter.simpledialog.askstring(title='修改密码',prompt='请输入新密码')
+        if len(newkey) != 16 and len(newkey) != 24 and len(newkey) != 32:
+            tkinter.messagebox.showwarning('警告','密钥长度非法')
+            os.remove('./temp.txt')
+            return
+        cbcmode = pyaes.AESModeOfOperationCBC(bytes(newkey,encoding="utf8"))
+        file_in = open('./temp.txt','rb')  #加b以二进制形式读写，bytes兼容py2、py3
+        file_out = open('./passwdency_cbc_change.txt', 'wb')
+        pyaes.encrypt_stream(cbcmode, file_in, file_out)
+        file_in.close()
+        file_out.close()
+        os.remove('./temp.txt')
+
+    def _cfbchangepw(self):
+        '''先使用旧密码解密'''
+        cfbbytekey = self.cfbentrykeyvar.get().encode('utf-8')
+        if len(cfbbytekey) != 16 and len(cfbbytekey) != 24 and len(cfbbytekey) != 32:
+            tkinter.messagebox.showwarning('警告','密钥长度非法')
+            return
+        cfbmode = pyaes.AESModeOfOperationCFB(cfbbytekey,
+                                              iv=None,
+                                              segment_size=8)
+        file_in = open(self.cfbentryfilevar.get(), 'rb')
+        file_out = open('./temp.txt', 'wb')
+        pyaes.decrypt_stream(cfbmode, file_in, file_out)
+        file_in.close()
+        file_out.close()
+        '''使用新密码加密'''
+        newkey = tkinter.simpledialog.askstring(title='修改密码',prompt='请输入新密码')
+        if len(newkey) != 16 and len(newkey) != 24 and len(newkey) != 32:
+            tkinter.messagebox.showwarning('警告','密钥长度非法')
+            os.remove('./temp.txt')
+            return
+        cfbmode = pyaes.AESModeOfOperationCFB(bytes(newkey,encoding="utf8"),
+                                              iv=None,
+                                              segment_size=8)
+        file_in = open('./temp.txt', 'rb')
+        file_out = open('./passwdency_cfb.txt', 'wb')
+        pyaes.encrypt_stream(cfbmode, file_in, file_out)
+        file_in.close()
+        file_out.close()
+        os.remove('./temp.txt')
 
 
 if __name__ == '__main__':
